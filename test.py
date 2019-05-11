edc64cb70658b2f7462013949607fb8d7d9b95f0
Yajat Vishwakk <yajat472001@gmail.com>
2019-05-11 22:48:48 +0530

    Update test.py

--- test.py
+++ test.py
@@ -9,19 +9,60 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
+    print("Welcome")
     req = request.get_json(silent=True, force=True)
     print("Request:")
     print(json.dumps(req, indent=4))
-    if req.get("queryResult").get("action") != "find.age":
-        return {}
+    if req.get("queryResult").get("action") == "find.age":
+        return findage()
+    elif req.get("queryResult").get("action") == "puzzles.yes":
+        return puzzleValidationYes()
+    elif req.get("queryResult").get("action") == "puzzles.no":
+        return puzzleValidationNo()
+    elif req.get("queryResult").get("action") == "puzzle.maybe":
+        return puzzleValidationMaybe()
     else:
-        res = {
-            "fulfillmentText": "This is a text response FUCKKKKKKKKKKKKKKKKKKKKKKKKKKK"
-        }
-        res = json.dumps(res, indent=4)
-        r = make_response(res)
-        r.headers['Content-Type'] = 'application/json'
-        return r
+        return {}
+        
+
+def findage():
+    req = request.get_json(silent= True, force= True)
+    age = req.get("queryResult").get("parameters").get("number")
+    if(age<10):
+        res = {"fulfillmentText": "You are a kid"}#scratch
+    else:
+        return {}
+    res = json.dumps(res, indent=4)
+    webhookreply = make_response(res)
+    webhookreply.headers['Content-Type'] = 'application/json'   
+    return webhookreply
+
+def puzzleValidationYes():
+     res = {"fulfillmentText": "C#/C++"}
+     res = json.dumps(res,indent=4)
+     webhookreply = make_response(res)
+     webhookreply.headers['Content-Type'] = 'application/json'
+     return webhookreply
+
+def puzzleValidationNo():
+     res = {"fulfillmentText": "Python"}
+     res = json.dumps(res,indent=4)
+     webhookreply = make_response(res)
+     webhookreply.headers['Content-Type'] = 'application/json'
+     return webhookreply
+
+def puzzleValidationMaybe():
+     res = {"fulfillmentText": "Java"}
+     res = json.dumps(res,indent=4)
+     webhookreply = make_response(res)
+     webhookreply.headers['Content-Type'] = 'application/json'
+     return webhookreply
+     
+
+
+			
+			
+			
 
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
